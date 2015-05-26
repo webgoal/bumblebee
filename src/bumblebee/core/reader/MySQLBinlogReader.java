0919d9eebd2f9e5ac311006ae4ef4bb16556ef62
Altieres Lopes <altiereslopes@webgoal.com.br>
2015-05-26 19:11:17 -0300

    minor

--- src/bumblebee/core/reader/MySQLBinlogReader.java
+++ src/bumblebee/core/reader/MySQLBinlogReader.java
@@ -1,9 +1,7 @@
 package bumblebee.core.reader;
 
 import java.io.Serializable;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
