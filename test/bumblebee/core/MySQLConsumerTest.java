569cafe50eeacbbdc2a4f3de5f73554f869d108b
Altieres Lopes <altiereslopes@webgoal.com.br>
2015-05-28 16:06:04 -0300

    test refactoring

--- test/bumblebee/core/MySQLConsumerTest.java
+++ test/bumblebee/core/MySQLConsumerTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import bumblebee.core.applier.MySQLConsumer;
@@ -14,37 +15,28 @@ public class MySQLConsumerTest {
 	private MySQLConsumer consumer = new MySQLConsumer();
 	private Event event = new Event();
 	private Map<String, Object> data = new HashMap<String, Object>();
-
-	@Test public void eventToSQLInsertTransformationTest() {
-		data.put("id", 1);
+	private Map<String, Object> conditions = new HashMap<String, Object>();
+	
+	@Before public void setup() {
 		event.setNamespace("database");
 		event.setCollection("table");
+		
+		data.put("id", 1);
 		event.setData(data);
+		
+		conditions.put("id", 2);
+		event.setConditions(conditions);
+	}
 
+	@Test public void eventToSQLInsertTransformationTest() {
 		assertEquals("INSERT INTO database.table SET id = '1'", consumer.prepareInsertSQL(event));
 	}
 	
 	@Test public void eventToSQLUpdateTransformationTest() {
-		Map<String, Object> conditions = new HashMap<String, Object>();
-
-		conditions.put("id", 2);
-		data.put("id", 1);
-		event.setData(data);
-		event.setConditions(conditions);
-		event.setNamespace("database");
-		event.setCollection("table");
-
 		assertEquals("UPDATE database.table SET id = '1' WHERE id = '2'", consumer.transformEventIntoUpdate(event));
 	}
 	
 	@Test public void eventToSQLDeleteTransformationTest() {
-		Map<String, Object> conditions = new HashMap<String, Object>();
-
-		conditions.put("id", 2);
-		event.setConditions(conditions);
-		event.setNamespace("database");
-		event.setCollection("table");
-
 		assertEquals("DELETE FROM database.table WHERE id = '2'", consumer.transformEventIntoDelete(event));
 	}
 
